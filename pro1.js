51031defba937741f5ae85c31e9b370dd5b00a6a
Aqsamalik110 <80627701+Aqsamalik110@users.noreply.github.com>
2021-05-25 09:41:26 -0700

    Add files via upload
    JS project1---color flipper

---
+++ pro1.js
@@ -0,0 +1,19 @@
+ const colors = ["red " , "yellow " ," pink " , "purple"];
+ const button = document.getElementById("button");
+
+const color = document.querySelector(".color");
+
+button.addEventListener("click", function(){
+const randomNumber = getRandomNumber();
+console.log(randomNumber);
+
+
+document.body.style.backgroundColor = colors[randomNumber];
+color.textContent = colors[randomNumber];
+});
+function getRandomNumber(){
+return Math.floor( Math.random()*colors.length);
+
+
+};
+  
